8eba03e4d8a5a6010daef38fc990f4fb1a4ede3a
YIHAN WANG <yw7451@nyu.edu>
2023-09-17 12:07:55 -0400

    begin/end shape

--- sketch.js
+++ sketch.js
@@ -1,5 +1,37 @@
 function setup() {
+    createCanvas(800, 800);
 }
 
 function draw() {
+    background("#E4DFC9");
+    translate(400, 400);
+    fill("#282828");
+    noStroke();
+    triangle(-400, -400, -195, -400, -400, -4);
+    beginShape();
+    vertex(-400, -4);
+    vertex(-197, -1);
+    vertex(-195, -195);
+    vertex(-96, -193);
+    vertex(0, 0);
+    vertex(-192, height - 400);
+    vertex(-400, height - 400);
+    endShape(CLOSE);
+    rotate(PI);
+    fill("#282828");
+    noStroke();
+    triangle(-400, -400, -195, -400, -400, -4);
+    beginShape();
+    vertex(-400, -4);
+    vertex(-197, -1);
+    vertex(-195, -195);
+    vertex(-96, -193);
+    vertex(0, 0);
+    vertex(-192, height - 400);
+    vertex(-400, height - 400);
+    endShape(CLOSE);
+    noFill();
+    stroke(255);
+    strokeWeight(5);
+    rect(-400,-400,width,height);
 }
